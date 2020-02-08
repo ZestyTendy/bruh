4c14e9fec463482784727f726956a0da4f929dd6
AnonymousFood <56987492+AnonymousFood@users.noreply.github.com>
2020-02-08 15:48:22 -0600

    Add files via upload

---
+++ frameProject/src/frameProject/Game.java
@@ -0,0 +1,63 @@
+package frameProject;
+import java.awt.*;
+import java.awt.color.*;
+import java.awt.event.*;
+import javax.swing.JFrame;
+
+public class Game extends Frame implements MouseListener {
+	private Button btn = new Button();
+	private Label l = new Label();
+	private mouseListener m = new mouseListener();
+	private Color c = new Color(50,50,50);
+	
+	public Game() {
+	}
+	
+	private void initMenu() {
+		// Initializing container
+        setVisible(true);
+        setTitle("Game");
+	    setLayout(null);
+	    setSize(1920,1080); 
+	    setBackground(c);
+	    
+	    // Closes application upon clicking X
+        addWindowListener(new WindowAdapter(){  
+        	public void windowClosing(WindowEvent e) {  
+        		dispose();  
+            }  
+        });
+        
+        // Adds Button
+		btn.setBounds(100,100,100,50);
+		btn.setLabel("click me");
+		btn.addMouseListener(this);
+		add(btn);
+	}
+
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		btn.setLabel("pressed");
+	}
+	@Override
+	public void mousePressed(MouseEvent e) {
+	}
+	@Override
+	public void mouseReleased(MouseEvent e) {
+	}
+	@Override
+	public void mouseEntered(MouseEvent e) {
+	}
+	@Override
+	public void mouseExited(MouseEvent e) {
+	}
+	
+	public static void main(String[] args) {
+		Game game = new Game();
+		game.initMenu();
+	}
+	
+}
+
+
---
+++ frameProject/src/frameProject/mouseListener.java
@@ -0,0 +1,22 @@
+package frameProject;
+
+import java.awt.*;  
+import java.awt.event.*;  
+public class mouseListener extends Frame implements MouseListener{  
+ 
+    mouseListener(){  
+        addMouseListener(this);   
+    }  
+    public void mouseClicked(MouseEvent e) {  
+    	
+    }  
+    public void mouseEntered(MouseEvent e) {  
+    }  
+    public void mouseExited(MouseEvent e) {  
+    }  
+    public void mousePressed(MouseEvent e) {  
+    }  
+    public void mouseReleased(MouseEvent e) {  
+    }  
+
+}  
