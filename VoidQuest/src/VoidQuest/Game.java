51116b9e92a0c3ce7ad42d3b704d2d0fcc86b5ca
ZestyTendy <57239266+ZestyTendy@users.noreply.github.com>
2020-02-08 19:54:27 -0600

    Update Game.java

--- VoidQuest/src/VoidQuest/Game.java
+++ VoidQuest/src/VoidQuest/Game.java
@@ -8,32 +8,40 @@ import java.awt.event.MouseListener;
 
 public class Game extends Frame implements MouseListener {
 	private Button menuButton = new Button();
-	private Driver window;
+	private Window window;
+	private boolean isOn = false;
 	
-	public Game(Driver x) {
+	public Game(Window x) {
 		window = x;
 		setBackground(new Color(50,50,50));
 		menuButton.setBounds(40,40,300,80);
 		menuButton.setLabel("Main Menu");
 		menuButton.addMouseListener(this);
 		x.add(menuButton);
+		off();
 	}
 	
 	public void on() {
 		menuButton.setEnabled(true);
 		menuButton.setVisible(true);
+		isOn = true;
 	}
 	
 	public void off() {
 		menuButton.setEnabled(false);
 		menuButton.setVisible(false);	
+		isOn = false;
+	}
+	
+	public boolean getActive() {
+		return isOn;
 	}
 
 	@Override
 	public void mouseClicked(MouseEvent e) {
 		if(e.getSource() == menuButton) {
 			off();
-			menu.on();
+			window.toggleInput();
 			
 		}
 	}
